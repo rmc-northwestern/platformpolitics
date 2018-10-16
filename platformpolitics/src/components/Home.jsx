02f0d96d97af797d8b03e708cab20c2d022ef400
Ryan McHenry <ryanmchenry2@gmail.com>
2018-10-15 19:45:50 -0500

    text update

--- platformpolitics/src/components/Home.jsx
+++ platformpolitics/src/components/Home.jsx
@@ -9,11 +9,11 @@ class Home extends Component {
     return (
       <div>
         <div className='homeContainer'>
-          <div className='homeTitle'>platform politics</div>
+          <div className='homeTitle'>platform politics.</div>
           <div className='homeSubtitle'>where what you say <br/>says you how you'll vote</div>
 
           <img className='homeImg' src='/img/bird.png' alt='home-content' />
-          
+
           <Link to='/elections'><div className='button1'>see current elections</div></Link>
           <Link to='/newelection'><div className='button1'>build new model</div></Link>
         </div>
