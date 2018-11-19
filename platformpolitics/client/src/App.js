229574ba19feea7618ee812cd58f934ffee9f3db
Ryan McHenry <ryanmchenry2@gmail.com>
2018-11-19 15:58:34 -0600

    removing unused files

--- platformpolitics/client/src/App.js
+++ platformpolitics/client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Headbar from './components/Headbar';
 import Home from './components/Home';
-import Customers from './components/Customers';
 
 class App extends Component {
   render() {
@@ -10,7 +9,6 @@ class App extends Component {
       <div>
         <Headbar/>
         <Home />
-        <Customers/>
       </div>
     );
   }
