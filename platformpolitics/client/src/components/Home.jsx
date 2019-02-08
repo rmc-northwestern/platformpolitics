ea1e698956ef52095fd5eb88172fab86347b289b
sdobon <sebastiandobon2@gmail.com>
2019-02-08 14:57:41 -0600

    formatting

--- platformpolitics/client/src/components/Home.jsx
+++ platformpolitics/client/src/components/Home.jsx
@@ -17,7 +17,9 @@ class Home extends Component {
           <Link to='/elections'><div className='button1'>predict a vote</div></Link>
           <Link to='/newelection'><div className='button1'>build new election model</div></Link>
           <div className='homeDescriptionHeading'>How it Works</div>
+          <div className='homeDescriptionSubHeading'>Predicting a vote</div>
           <div className='homeDescription'>Predictive Politics allows you to predict how a given Twitter user would vote in an election.  You first choose an election, then give a Twitter handle. Predictive Politics downloads their latest tweets and uses natural language processing machine learning techniques to predict their vote in the chosen election.</div>
+          <div className='homeDescriptionSubHeading'>Building a new model</div>
           <div className='homeDescriptionLast'>If you don't see the election you want, you can create a new one by giving the Twitter handles of two candidates. Prediction Politics will then automatically collect data from Twitter and use it to build a model of that election.</div>
         </div>
       </div>
